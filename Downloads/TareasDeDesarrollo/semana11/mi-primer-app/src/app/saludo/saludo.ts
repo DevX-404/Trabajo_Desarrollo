acecf578182dcf61a9170079c0086186bab15f56
Luis Bances <sergio5416v@gmail.com>
2026-06-03 19:23:29 -0500

    Trabajo 3 de del decorador Component

---
+++ Downloads/TareasDeDesarrollo/semana11/mi-primer-app/src/app/saludo/saludo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saludo',
+  imports: [],
+  templateUrl: './saludo.html',
+  styleUrl: './saludo.css',
+})
+export class Saludo {
+  integrantes: string = 'Equipo de la pregunta 3';
+}
